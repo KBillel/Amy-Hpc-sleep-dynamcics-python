8fcb63e83c2897fbb57d8e441cb09ad0815539ee
Billel Khouader <billel.khouader@adifm.int>
2020-12-08 12:30:31 +0100

    update .gitignore

--- bk/.ipynb_checkpoints/signal-checkpoint.py
+++
@@ -1,8 +0,0 @@
-import scipy
-import neuroseries as nts
-import numpy as np
-
-def passband(lfp,low,high,fs = 1250,order = 4):
-    b,a = scipy.signal.butter(order,[low, high],'band',fs = 1250)
-    filtered = scipy.signal.filtfilt(b,a,lfp.values)
-    return nts.Tsd(np.array(lfp.index),filtered)
